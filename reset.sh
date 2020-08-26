467aa7f6f4437f6f65c5e2e698e45a4036338be4
Hugothms <hugothms@gmail.com>
2020-08-26 17:24:32 +0200

    az

--- reset.sh
+++ reset.sh
@@ -1,7 +1,8 @@
+minikube stop
 kubectl delete deployments --all
 kubectl delete service --all
 kubectl delete namespaces metallb-system
-docker rmi -f my-nginx alpine
+docker rmi -f my_nginx alpine
 # docker system prune -a
 # docker rmi $(docker images -a -q)
 # minikube delete
