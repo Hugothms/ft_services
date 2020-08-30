6a0f2dc7e17f63333e8ef604b173345fee0af9ea
Hugothms <hugothms@gmail.com>
2020-08-30 16:03:16 +0200

    move deployments

--- setup.sh
+++ setup.sh
@@ -14,6 +14,9 @@ eval $(minikube docker-env)
 # minikube status
 # kubectl version
 
+IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
+printf "Minikube IP: ${IP}"
+
 # ! MetalLB
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
