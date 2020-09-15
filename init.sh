3517a92be0ee38037fcf5fb55882ad2baca207a1
Hugothms <hugothms@gmail.com>
2020-09-15 16:22:41 +0200

    init.sh mac

--- init.sh
+++ init.sh
@@ -1,1 +1,7 @@
-service nginx stop
+brew install kubectl
+brew install minikube
+minikube config set vm-driver virtualbox
+minikube delete
+docker-machine create --driver virtualbox default
+docker-machine start
+minikube start --vm-driver=virtualbox
