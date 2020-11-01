6dd732e5e4833c21cfcff9eede6f9aec419ffd6b
Hugothms <hugothms@gmail.com>
2020-11-01 08:39:42 +0100

    clean

--- srcs/mysqlcopy/start.sh
+++ srcs/mysqlcopy/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 # nohup ./init_mysql.sh > /dev/null 2>&1 &
 
-sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
-telegraf &
+# sed -i 's/skip-networking/#skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+# telegraf &
 mysql_install_db --user=mysql
 mysqld_safe
 
