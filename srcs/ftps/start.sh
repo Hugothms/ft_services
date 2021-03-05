6353dc8b37c729c739e1602dc2830ec8e0d0a037
hugothms <hugothms@gmail.com>
2021-03-05 16:42:49 +0100

    chage vsftpd conf

--- srcs/ftps/start.sh
+++ srcs/ftps/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-USER=ftp
-PASSWORD=alpineftp
+USER=admin
+PASSWORD=admin
 FOLDER="/ftp"
 ADDR=$(cat ip)
 
@@ -12,11 +12,11 @@ chown $USER:$USER $FOLDER
 # grep '/ftp/' /etc/passwd | cut -d':' -f1 | xargs -n1 deluser
 unset USER PASSWORD FOLDER UID
 
+echo "this is a test file" > /home/user42/ftp/files/test.txt
 # echo "set ssl:verify-certificate no" >> ~/.lftp/rc
 
 telegraf &
-exec /usr/sbin/vsftpd -opasv_address=$ADDR /etc/vsftpd/vsftpd.conf
-sh
+exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
 
 # set ssl:verify-certificate no
 # lftp -d -u ftp,alpineftp 192.168.49.2 
